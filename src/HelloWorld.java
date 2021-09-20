5d32299be7fc2a965438e5dd5d0a2bc7255d909c
Ian McConihay <mcconihay.ian92@gmail.com>
2021-09-20 12:08:15 -0500

    Java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,11 @@
 public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Ian");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myFirstThird = 6;
+        System.out.println(myFirstNumber + mySecondNumber + myFirstThird);
+    }
 }
