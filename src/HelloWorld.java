958f76712e23e02f2cc32545fe86c0f5aecc83c8
Ian McConihay <mcconihay.ian92@gmail.com>
2021-10-05 11:51:48 -0500

    int types

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,14 +1,23 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Ian");
+//        System.out.println("Hello, Ian");
 
-        int myFirstNumber = (10 + 5) + (2 * 10);
-        int mySecondNumber = 12;
-        int myFirstThird = myFirstNumber * 2;
-        int myTotal = myFirstNumber + mySecondNumber + myFirstThird;
-        System.out.println(myTotal);
-        int myLastOne = 1000 - myTotal;
-        System.out.println(myLastOne);
+//        int myFirstNumber = (10 + 5) + (2 * 10);
+//        int mySecondNumber = 12;
+//        int myFirstThird = myFirstNumber * 2;
+//        int myTotal = myFirstNumber + mySecondNumber + myFirstThird;
+//        System.out.println(myTotal);
+//        int myLastOne = myTotal - 1000;
+//        System.out.println(myLastOne);
+    int  myValue = 10000;
+    int  myMinIntValue = Integer.MIN_VALUE;
+    int  myMaxIntValue = Integer.MAX_VALUE;
+    System.out.println("Interger Minimum Value = '" + myMinIntValue);
+    System.out.println("Interger Maximum Value = '" + myMaxIntValue);
+    System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
+    System.out.println("Busted MIN value = " + (myMinIntValue - 1));
+
+    int myMaxIntTest = 2_147_483_647;
     }
 }
