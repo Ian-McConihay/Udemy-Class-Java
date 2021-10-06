ab2a69b3d878b3f669e666dda8a6551fd1463c53
Ian McConihay <mcconihay.ian92@gmail.com>
2021-10-05 23:38:26 -0500

    if/then cont.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -157,9 +157,15 @@ public class HelloWorld {
         System.out.println("And I am scared of aliens");
     }
 
+    int topScore = 80;
+    if(topScore < 100) {
+        System.out.println("you got the high score!");
+    }
 
-
-
+    int secondTopScore = 60;
+    if(topScore > secondTopScore && topScore < 100) {
+        System.out.println("Greater than second top score and less than 100");
+    }
 
 
 
