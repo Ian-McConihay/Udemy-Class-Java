7c292c8151d69b2b61a8458c7ecf83606ff2711a
Ian McConihay <mcconihay.ian92@gmail.com>
2021-10-05 23:24:45 -0500

    operators cont

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -137,7 +137,18 @@ public class HelloWorld {
         System.out.println("20 / 5 = " + result);
         result = result % 3;
         System.out.println("4 % 3 = " + result);
-
+        result++;
+        System.out.println("1 + 1 = " + result);
+        result--;
+        System.out.println("2 - 1 = " + result);
+        result += 2;
+        System.out.println("1 + 2 = " + result);
+        result *= 10;
+        System.out.println("3 * 10 = " + result);
+        result /= 3;
+        System.out.println("30 / 3 = " + result);
+        result -= 2;
+        System.out.println("10 - 2 = " + result);
 
 
 
