f21ed0dfd5740de3d39e02d5f457c8cbe9c4edf6
Ian McConihay <mcconihay.ian92@gmail.com>
2021-10-31 16:17:06 -0500

    Java udemy

--- src/Section4.java
+++ src/Section4.java
@@ -1,2 +1,67 @@
 public class Section4 {
+	public static void main(String[] args) {
+//		Expression
+		double kilometers = (100 * 1.609344);
+		int highScore = 100;
+//		Control flow statement
+//		If statement
+		if (highScore > 99){
+			System.out.println("You got a high score!");
+			highScore = 0;
+				}
+
+//			Java statement
+
+//			int
+//					myVariable
+//					=
+//					50;
+		int myVariable = 50;
+		myVariable++;
+		System.out.println("This is a test " +
+				"Another test" +
+				" and another ");
+		//Both valid complete statements
+		int anotherVariable=50;myVariable--;System.out.println("This is another");
+
+
+
+
+
+//		CODE BLOCKS
+
+		boolean gameOver =true;
+		int score = 5000;
+		int levelCompleated = 5;
+		int bonus = 100;
+		if(score == 5000) {
+			System.out.println("Your Score is 5000");
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
 }
