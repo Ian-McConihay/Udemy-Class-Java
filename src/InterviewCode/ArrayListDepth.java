bef6db434e2bc052c457e3e759c9272d306808da
Ian McConihay <mcconihay.ian92@gmail.com>
2022-01-22 17:50:52 -0600

    ArrayList

---
+++ src/InterviewCode/ArrayListDepth.java
@@ -0,0 +1,22 @@
+package InterviewCode;
+
+import java.util.ArrayList;
+
+public class ArrayListDepth {
+	public static void main(String[] args) {
+//This is an array
+		String[] cars = new String[3];
+		cars[0] = "Honda";
+		cars[1] = "Toyota";
+		cars[2] = "Acura";
+		System.out.println(cars);
+
+
+		ArrayList carList = new ArrayList();
+		carList.add("honda");
+		carList.add("toyota");
+		carList.add("acura");
+
+	}
+
+}
