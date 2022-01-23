36b534d71ca200d15aa96333f28a4f5d17acac5f
Ian McConihay <mcconihay.ian92@gmail.com>
2022-01-23 10:26:06 -0600

    fibbonaccitwo

--- src/InterviewCode/Fibonacci.java
+++ src/InterviewCode/Fibonacci.java
@@ -12,11 +12,20 @@ public class Fibonacci {
 		return fibonacci[num];
 	}
 
-//																		 1,2,3,4,5,6,7 ,8 ,9 ,10
+	private static long fibonacciTwo(int num){
+//		base case
+		if(num <= 1){
+			return num;
+		}
+		return (fibonacciTwo(num - 1) + fibonacciTwo(num - 2));
+	}
+
 //	So the fibonacci is the sum of the last two numbers in sequence... 0,1,1,2,3,5,8,13,21,34,55
 //	The method takes in an int that will return the index of the sequence.
+//	if you start to get negative numbers its because longs or ints cant go past a certain num
 	public static void main(String[] args) {
-	int n = 10;
+	int n = 3;
 		System.out.println(fibonacci(n));
+		System.out.println(fibonacciTwo(n));
 	}
 }
