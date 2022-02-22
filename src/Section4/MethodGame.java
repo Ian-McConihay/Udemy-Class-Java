baa06c6543d6f0d9e6853b9c638eab7f3436f0b1
Ian McConihay <mcconihay.ian92@gmail.com>
2022-02-21 20:02:52 -0600

    working on methods

--- src/Section4/MethodGame.java
+++ src/Section4/MethodGame.java
@@ -2,18 +2,18 @@ package Section4;
 
 public class MethodGame {
 	public static void main(String[] args) {
-		calculateScore(1000, true, levelCompleted, bonus);
-
-	}
-
-	public static void calculateScore(int score, boolean gameOver, int levelCompleted, int bonus){
-		boolean gameOver =true;
-		int highScore = 10_000;
+		boolean gameOver = true;
 		int score = 10_000;
 		int levelCompleted = 8;
 		int bonus = 200;
-		int finalScore = score + (levelCompleted * bonus);
 
+		calculateScore(gameOver, score, levelCompleted, bonus);
+		calculateScore(true, 1000, 10, 400);
+	}
+
+	public static void calculateScore(boolean gameOver, int score,  int levelCompleted, int bonus){
+		int finalScore = score + (levelCompleted * bonus);
+		int highScore = 10_000;
 		if(gameOver){
 			System.out.println("Your final score was " + finalScore);
 		} else if(finalScore > highScore && gameOver){
